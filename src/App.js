37737de0bdffd4ff5fbde3d9f189a70d9db1009c
Zain Saleem <zain.saleem155@gmail.com>
2026-05-12 18:11:33 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Navbar from "./components/Navbar/Navbar";
-import Footer from "./components/Footer/Footer";
+// import Footer from "./components/Footer/Footer";
 import HeroBanner from "./components/Hero/Hero";
 import FeatureCard from "./components/FeatureCards/FetureCard";
 
@@ -9,7 +9,7 @@ function App() {
       <Navbar />
       <HeroBanner/>
       <FeatureCard/>
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 }
